2e334dd232feb2771d17c3c569299c54aaa073b8
Lalitha Bhat <lalitha.bhat11@gmail.com>
2023-09-23 15:57:35 +0530

    fixed the rca main file

--- main_rca.py
+++ main_rca.py
@@ -1,30 +1,30 @@
 import streamlit as st
 import pandas as pd
-import indexes
+import indexes_rca
 
 col1, col2 = st.columns([3,2])
 
-offering_data_df = pd.DataFrame({
-        "Offering": ["Service Offerings", "Source"],
+rca_data_df = pd.DataFrame({
+        "Cause": ["Cause Analysis", "Source"],
         "Value": ["", ""]
 })
 
 with col1:
-    st.title("Lead routing engine")
-    lead_form = st.text_area("describe your requirements here", height=300)
-    if st.button("Search Offering"):
-        if not lead_form:
-            st.error('fill the lead form and then click search')
+    st.title("RCA Tool")
+    rca_form = st.text_area("describe your issue here", height=300)
+    if st.button("Identity Root Cause and Suggest Preventive Measures"):
+        if not rca_form:
+            st.error('fill the  form and then click search')
             st.stop()
         else:
-            offering_data_df = indexes.searchServices(lead_form)
+            rca_data_df = indexes_rca.searchServices(rca_form)
 
 with col2:
     st.markdown("<br/>" * 5, unsafe_allow_html=True)  # Creates 5 lines of vertical space
     st.dataframe(
-        offering_data_df,
+        rca_data_df,
         column_config={
-            "Offering": st.column_config.Column(width=150),
+            "Cause": st.column_config.Column(width=150),
             "Value": st.column_config.Column(width=150)
         },
         hide_index=True
